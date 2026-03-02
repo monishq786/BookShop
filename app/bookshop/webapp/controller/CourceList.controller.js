824c8e7906c98507562bba390cf2332c46749d77
Moh Monish <mohdmonish8882@gmail.com>
2026-03-02 04:34:09 +0000

    .

--- app/bookshop/webapp/controller/CourceList.controller.js
+++ app/bookshop/webapp/controller/CourceList.controller.js
@@ -8,20 +8,47 @@ sap.ui.define([
     return Controller.extend("bookshop.controller.CourceList", {
 
         onInit: function () {
-            // In V4, data flows automatically once the view is loaded
+            // let oData = {
+            //     Employee: [{
+            //         Name: "Monish",
+            //         Role: "Manager"
+            //     }, {
+            //         Name: "Alex",
+            //         Role: "Manager"
+            //     }]
+            // }
+            // let oModel =  new sap.ui.model.JSONModel(oData);
+            // this.getView().setModel(oModel, "EmpModel");
         },
 
-        onRefresh: function () {
-            // To refresh a V4 list, you refresh the binding, not the model
-            var oList = this.byId("idProductList");
-            var oBinding = oList.getBinding("items");
-
-            if (oBinding.hasPendingChanges()) {
-                MessageToast.show("Please save changes before refreshing.");
-                return;
+        openDialog: function () {
+            if (this._pDialog) { // this ensure this is loading first time its Promise Object
+                this._pDialog = this.loadFragment({ // to ensure load the fragment that if load first time the create new instance if opening secoind time reuse porevious instance bcz of memory leak
+                    name: "my.app.view.fragments.MyFragment"
+                }).then(function (oDialog) {
+                    this.getView().addDependant(oDialog) // load the model property into dialog or model this called view child, this is lifecycle managment process
+                    return oDialog
+                }).bind(this); // ensure that this is controller context preserved as parent 
             }
 
-            oBinding.refresh();
+            this._pDialog.then(function (oDialog) {
+                oDialog.open(); // open the dialog 
+            })
+        },
+
+        onExit: function () {
+            if (this._pDialog) { // this will check if view exit then dialog should be destroy with view 
+                this._pDialog.then(function (oDialog) {
+                    oDialog.destroy();
+                })
+            }
+        },
+
+        onSearchData: function (oEvent) {
+            let sQuery = oEvent.getParameters("newValue");
+            let oFilter = new sap.ui.model.Filter("Name", sap.ui.mode.FilterOperator.Contains, sQuery);
+            let oBinding = this.byId("myTable").getBinding("items");
+            oBinding.filter([oFilter]);
         }
     });
 });
